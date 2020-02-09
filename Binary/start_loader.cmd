240d4b077afda6765aec94b8dccf8534a6294423
hfiref0x <hfiref0x@users.noreply.github.com>
2020-02-09 15:08:37 +0700

    v 2.0.1
    Implementing some of the tips from #58

---
+++ Binary/start_loader.cmd
@@ -0,0 +1,4 @@
+REM Append full patch to loader.exe and run this batch file elevated
+@echo
+echo Running loader
+loader.exe
---
+++ Binary/stop_loader.cmd
@@ -0,0 +1,4 @@
+REM Append full patch to loader.exe and run this batch file elevated
+@echo
+echo Stopping loader
+loader.exe /s
